170bb5614c0c105ab7ee5b85bb466ec49e5985de
IcarusVi <zscyrus31@gmail.com>
2020-09-24 15:23:44 -0400

    Finished the css and skeleton of addExercise

--- no-bull-client/src/components/addExercise.js
+++ no-bull-client/src/components/addExercise.js
@@ -1,21 +1,83 @@
 import React, { Component } from 'react'
 
 export default class AddExercise extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+          routineName: "",
+          routineExercises: [],
+          exerciseTitle: '',
+          Sets: 0,
+          Reps: 0,
+          active:true
+        };
+      }
+      
+      handleChange = (e) =>{
+        this.setState({
+          routineName: e.target.value
+        })
+      }
+      
+      inputChange = (e) =>{
+        let name = e.target.name;
+        let val = e.target.value
+        this.setState({
+          [name]: val
+        })
+      }
+      
+      addExercise = () =>{
+        event.preventDefault();
+        let newExercise = {
+          title: this.state.exerciseTitle,
+          sets: this.state.Sets,
+          reps: this.state.Reps
+        }
+        console.log(newExercise);
+        this.setState({
+          routineExercises: [...this.state.routineExercises, newExercise],
+          exerciseTitle: '',
+          sets: 0,
+          reps: 0
+        })
+        
+      }                                              
+      
+      render() {
         return (
-            <div className="createExercise">
-                <h1>Create Routine</h1>
-                <div id="routineName">
-                    <ul id='routineList'>
-                    </ul>
-                </div>
-                <form>
-                    <input type='text'></input>
-                    <input type='text' name="Exercise Name" placeholder="Enter Exercise Name"/><br/>
-                    <input type='text' name="Sets" placeholder="Sets"/><br/>
-                    <input type='text' name="Reps" placeholder="Reps"/><br/>
-                </form>
+          <div className="createExercise">
+            <h1>Create Routine</h1>
+            <form>
+              <input type="text" onChange={this.handleChange} name='Routine Name' placeholder='Enter routine name'/>
+            </form>
+    
+            <div id="routineName">
+              <h2>{this.state.routineName}</h2>
+              <ul id="routineList">
+                {this.state.routineExercises.map((x)=>{
+                  return(
+                    <li>{x.title} Sets: {x.sets} Reps: {x.reps}</li>
+                  )
+                })}
+              </ul>
             </div>
-        )
-    }
+            <form>
+              <input
+                type="text"
+                name="exerciseTitle"
+                placeholder="Enter Exercise Name"
+                onChange={this.inputChange}
+              />
+              <br />
+              <input type="text" name="Sets" placeholder="Sets" onChange={this.inputChange} />
+              <br />
+              <input type="text" name="Reps" placeholder="Reps" onChange={this.inputChange} />
+              <br />
+              <button onClick={this.addExercise}><i className="fas fa-plus"></i></button>
+            </form>
+            <button id="sub-routine">Submit Routine</button>
+          </div>
+        );
+      }
 }
